d54a2ba32632a4ef210384f63bb8cd12ac420932
o.semenov <oleksii.semenov.25@kse.org.ua>
2026-04-15 00:17:25 +0300

    made tasks 6-8, josn file was created, add maplotlib and graph

--- main assignment.py
+++ main assignment.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 df = pd.read_csv("random_walk.csv")
 def distance(x,y):
     return (x**2+y**2)**0.5
@@ -7,4 +8,10 @@ df['distance'] = distance(df['x'],df['y'])
 max_distance = round(np.max(df['distance']), 2)
 mean_distance = round(np.mean(df['distance']), 2)
 print(f'the longest walk is {max_distance}, the mean distance is {mean_distance}')
-
+critical_deviation = df[df['distance'] > mean_distance]
+critical_deviation.to_json('filtered_walk.json', orient='records', indent=4)
+plt.figure(figsize=(8,4))
+plt.plot(df['x'], df['y'], color='green')
+plt.scatter(df['x'].iloc[0], df['y'].iloc[0], color='blue')
+plt.scatter(df['x'].iloc[-1], df['y'].iloc[-1], color='red')
+plt.show()
